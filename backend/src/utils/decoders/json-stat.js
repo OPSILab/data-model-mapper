e7ad958aa8bb0aa56cc3d4f0842eaaf0f5f54092
Gabriele Percoco <gabriele.percoco@demetrix.it>
2026-03-18 17:34:12 +0100

    update decoder

--- backend/src/utils/decoders/json-stat.js
+++ backend/src/utils/decoders/json-stat.js
@@ -7,6 +7,51 @@ const { Logger } = log
 const logger = new Logger(__filename)
 const Datapoints = require('./Datapoint');
 const Output = require('../../server/api/models/output');
+const translations = load8thPage()
+
+function codeFound(dimensions, map) {
+  //dimensions è un oggetto
+  let match = ""
+  for (let key in dimensions) {
+    //logger.debug(key, " ", dimensions[key])
+    for (let excelKey in map) {
+      //logger.debug(excelKey, map[excelKey].name)
+      if (
+        (map[excelKey].name == dimensions[key])
+        ||
+        (map[excelKey].name.toLowerCase() == dimensions[key].toLowerCase())
+        ||
+        (map[excelKey].name.split("/").find(part => part === dimensions[key]))
+        ||
+        (map[excelKey].name.split("/").find(part => part.toLowerCase() === dimensions[key].toLowerCase()))
+      )
+        return "NUTS" + map[excelKey].level.toString()
+      else if (map[excelKey].name.includes(dimensions[key]) || map[excelKey].name.toLowerCase().includes(dimensions[key].toLowerCase()))
+        match += "NUTS" + map[excelKey].level.toString() + "-" + dimensions[key]
+    }
+  }
+  if (match)
+    for (let key in dimensions)
+      if (translations[dimensions[key]])
+        for (let excelKey in map) {
+          //logger.debug(excelKey, map[excelKey].name)
+          if (
+            (map[excelKey].name == translations[dimensions[key]])
+            ||
+            (map[excelKey].name.toLowerCase() == translations[dimensions[key]].toLowerCase())
+            ||
+            (map[excelKey].name.split("/").find(part => part === translations[dimensions[key]]))
+            ||
+            (map[excelKey].name.split("/").find(part => part.toLowerCase() === translations[dimensions[key]].toLowerCase()))
+          )
+            return "NUTS" + map[excelKey].level.toString()
+          else if (map[excelKey].name.includes(translations[dimensions[key]]) || map[excelKey].name.toLowerCase().includes(translations[dimensions[key]].toLowerCase()))
+            match += "NUTS" + map[excelKey].level.toString() + "-" + translations[dimensions[key]]
+        }
+  if(!match)
+    return "NUTS0 ?"
+  return match
+}
 
 function loadNutsMap() {
   const workbook = xlsx.readFile(NUTS_XLSX);
@@ -24,6 +69,26 @@ function loadNutsMap() {
     const code = row[geoIndex];
     if (code) {
       map[code] = { name: row[labelIndex], level: row[levelIndex] };
+    }
+  }
+  return map;
+}
+
+function load8thPage() {
+  const workbook = xlsx.readFile(NUTS_XLSX);
+  const sheet = workbook.Sheets[workbook.SheetNames[8]];
+  const rows = xlsx.utils.sheet_to_json(sheet, { header: 1 });
+
+  const header = rows[0];
+  const labelIndex = header.indexOf("Label");
+  const transliterationToLatinIndex = header.indexOf("NUTS label");
+
+  const map = {};
+  for (let i = 1; i < rows.length; i++) {
+    const row = rows[i];
+    const translated = row[transliterationToLatinIndex];
+    if (translated) {
+      map[translated] = row[labelIndex];
     }
   }
   return map;
@@ -121,14 +186,14 @@ module.exports = async function decode(source, id) {
 
         if (nutsMap[code]?.level != null) {
           regionLevel = "NUTS" + nutsMap[code].level;
-        } else if (isRegional) {
+        } /*else if (codeFound(, nutsMap)){//(isRegional) {
           if (code.length === 3) regionLevel = "NUTS1";
           else if (code.length === 4) regionLevel = "NUTS2";
           else if (code.length === 5) regionLevel = "NUTS3";
           else regionLevel = "NON_NUTS";
         } else {
-          regionLevel = "NON_NUTS";
-        }
+          regionLevel = "NON_NUTS" + code;
+        }*/
       }
     }
 
@@ -155,6 +220,9 @@ module.exports = async function decode(source, id) {
         });
         purged = true;
       }*/
+      if (record.region == "unknown")
+        record.region = codeFound(record.dimensions, nutsMap)
+      record.dimensions = Object.values(record.dimensions)
       bufferArray.push(record)
       if (bufferArray.length >= config.batch) {
         if (config.sessionLocation.mongo)
